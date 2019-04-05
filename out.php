a636aa0a43fd808d6ba30777b46d4f8c74b33e8d
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-04-06 02:15:56 +0800

    清空

--- out.php
+++
@@ -1,6 +0,0 @@
-<?php
-	userdata($_SESSION["u"]);
-	itemlist($_SESSION["c"], 0);
-?>
-<input type="button" value="確定送出" onclick="lof('api.php?do=chkout')" >
-<input type="button" value="返回修改訂單" onclick="lof('?do=buycart')" >
